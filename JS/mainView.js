56bcbcebbe411cbec1ba720a0b0bde0dc58b31a3
chrissundal <christoffersj@hotmail.com>
2024-10-07 20:11:06 +0200

    last

--- JS/mainView.js
+++ JS/mainView.js
@@ -18,8 +18,8 @@ function createHeader() {
     let html = '';
     html = `
     <div class="header" onclick="goHomeButton()">
-            <img src="IMG/kaiamovies.png" height = 80px>
-        </div>
+        <img src="IMG/kaiamovies.png" height = 80px>
+    </div>
     `;
     return html;
 }
@@ -37,33 +37,26 @@ function createDropdownMovie() {
 }
 function createMainMovieList() {
     let html = '';
-    const unique = getUniqueMovies();
-    for(let index = 0; index < model.input.mainPage.uniqueMovies.length; index++){
-
-        html += `
-        <div class="movieBox" onclick="goMovie('${model.input.mainPage.uniqueMovies[index].name}')">
-        <div class="movieRating">${model.input.mainPage.uniqueMovies[index].avgRating}</div>
-        <br>
-        <img src="${model.input.mainPage.uniqueMovies[index].movieImage}" height="130px" width="90px"/>
-        <br>
-        <div class="movieText">${model.input.mainPage.uniqueMovies[index].name}</div>
-        </div>
-        `;
-        }
-            
-    return html;
-}
-
-
-function getUniqueMovies() {
     const allMovies = model.data.movies;
     const filteredMovies = allMovies.filter(movie => !model.data.users[model.input.profile.selectedUser].watchlist.find(m => m.name === movie.name) && movie.avgRating >= 800);
-    
+
     while (model.input.mainPage.uniqueMovies.length < 4 && filteredMovies.length > 0) {
         const randomIndex = Math.floor(Math.random() * filteredMovies.length);
         model.input.mainPage.uniqueMovies.push(filteredMovies[randomIndex]);
         filteredMovies.splice(randomIndex, 1);
     }
 
-    return model.input.mainPage.uniqueMovies;
+    for (let index = 0; index < model.input.mainPage.uniqueMovies.length; index++) {
+        html += `
+        <div class="movieBox" onclick="goMovie('${model.input.mainPage.uniqueMovies[index].name}')">
+            <div class="movieRating">${model.input.mainPage.uniqueMovies[index].avgRating}</div>
+            <br>
+            <img src="${model.input.mainPage.uniqueMovies[index].movieImage}" height="130px" width="90px"/>
+            <br>
+            <div class="movieText">${model.input.mainPage.uniqueMovies[index].name}</div>
+        </div>
+        `;
+    }
+
+    return html;
 }
